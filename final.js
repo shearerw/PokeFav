4308809ea737a94e721783e00ca7afd6a8a1cb7e
William Shearer <wshearer@umd.edu>
2022-05-10 20:23:30 -0400

    End of 05/10/2022

--- final.js
+++ final.js
@@ -9,27 +9,25 @@ let bodyParser = require("body-parser");
 let fetch = require("node-fetch-commonjs");
 const { response } = require("express");
 require("dotenv").config({ path: path.resolve(__dirname, "env/.env") });
-//import fetch from 'node-fetch';
 
-let url = "https://pokeapi.co/api/v2/pokemon/greedent";
+let url = "https://pokeapi.co/api/v2/pokemon/";
 
-/*fetch(url)
-  .then((response) => response.json())
-  .then((json) => processObject(json))
-  .catch((error) => console.log("Reporting error: " + error));
-
-function processObject(json) {
-  // Our response is an array of values
-  console.log("\n\n***** Values Received *****\n");
-  //console.log(json);
-  console.log(json.name);
-  console.log(json.id);
-  console.log(
-    `name: ${json.name} pokedex number: ${json.id} height: ${json.height} weight: ${json.weight}`
-  );
-  console.log(json.sprites.front_default);
-  //json.forEach(entry => console.log(entry.title));
-}*/
+const userName = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+const database = process.env.MONGO_DB_NAME;
+const collection = process.env.MONGO_COLLECTION;
+const databaseAndCollection = {
+  db: database,
+  collection: collection,
+};
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { table } = require("console");
+const uri = `mongodb+srv://${userName}:${password}@cluster0.srgzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 if (process.argv.length != 3) {
   console.log(`Usage summerCampServer.js PORT_NUMBER`);
@@ -45,50 +43,66 @@ app.get("/", function (request, response) {
   response.render("index");
 });
 
+//-------search and add one pokemon-------------------------------------------
 app.get("/search", function (request, response) {
-    let variables = { port: portNumber, port1: portNumber };
-    response.render("search", variables);
+  let variables = { port: portNumber, port1: portNumber };
+  response.render("search", variables);
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.post("/searchConfirmation", function (request, response) {
-  /*let variables = {
-    name: request.body.name,
-    email: request.body.email,
-    gpa: request.body.gpa,
-    backInfo: request.body.backInfo,
-    date: new Date(),
-    port: portNumber,
-  };*/
-  let variables ={
-    name: "5",//json.name,
-    date: "4",//new Date(),
-    port: "3"//portNumber
-  }
+  let variables = {
+    name: "5", //json.name,
+    date: "4", //new Date(),
+    port: "3", //portNumber
+  };
+  url = "https://pokeapi.co/api/v2/pokemon/" + request.body.name;
   fetch(url)
-  .then((response) => response.json())
-  //.then((json) => processSearch(json))
-  .then((json) => {
-      console.log(json.name);
-      variables = {
-        name: json.name,
-        date: new Date(),
-        port: portNumber
+    .then((response) => response.json())
+    //.then((json) => processSearch(json))
+    .then((json) => {
+      if (json == undefined) {
+        //  console.log(json);
+        portOnly = { port: portNumber };
+        response.render("notFound", portOnly);
+      } else {
+        //console.log("your mom");
+        //console.log(json.name);
+        variables = {
+          name: json.name,
+          dex: json.id,
+          height: json.height,
+          weight: json.weight,
+          front: json.sprites.front_default,
+          date: new Date(),
+          port: portNumber,
+        };
+        ins(variables);
+        response.render("searchConfirmation", variables);
       }
-      response.render("searchConfirmation", variables);
+    })
+    .catch((error) => {
+      portOnly = { port: portNumber };
+      response.render("notFound", portOnly);
     });
-  //.catch((error) => console.log("Reporting error: " + error));
-
-  /*let camper = {
-    name: request.body.name,
-    email: request.body.email,
-    gpa: request.body.gpa,
-    backInfo: request.body.backInfo,
-  };*/
-  //ins(camper);
-  
-  //response.render("searchConfirmation", variables);
 });
+async function ins(camper) {
+  try {
+    await client.connect();
+
+    await insertCamper(client, databaseAndCollection, camper);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+async function insertCamper(client, databaseAndCollection, newCamper) {
+  const result = await client
+    .db(databaseAndCollection.db)
+    .collection(databaseAndCollection.collection)
+    .insertOne(newCamper);
+}
 
 function processSearch(json) {
   // Our response is an array of values
@@ -100,15 +114,105 @@ function processSearch(json) {
     `name: ${json.name} pokedex number: ${json.id} height: ${json.height} weight: ${json.weight}`
   );
   console.log(json.sprites.front_default);
-  let variables ={
-      name: "5",//json.name,
-      date: "4",//new Date(),
-      port: "3"//portNumber
-  }
+  let variables = {
+    name: "5", //json.name,
+    date: "4", //new Date(),
+    port: "3", //portNumber
+  };
   //response.render("searchConfirmation", variables);
   //json.forEach(entry => console.log(entry.title));
 }
 
+//-------view as table--------------------------------------------------------
+app.get("/viewAll", function (request, response) {
+  console.log("here");
+  let p3 = (async () => {
+    console.log("here5");
+    try {
+      console.log("here 2");
+      await client.connect();
+      let filter = {};
+      const cursor = client
+        .db(databaseAndCollection.db)
+        .collection(databaseAndCollection.collection)
+        .find(filter);
+      let tableHTML = `<table border="1">`;
+      const result = await cursor.toArray();
+      console.log(`Found: ${result.length} movies`);
+      result.forEach((ele) => {
+        tableHTML += `<tr><td rowspan="2"><img src="${ele.front}"</td><td><strong>name:</strong> ${ele.name}</td><td><strong>number:</strong> ${ele.dex}</td></tr><tr><td><strong>height:</strong> ${ele.height}</td><td><strong>weight:</strong> ${ele.weight}</td></tr>`;
+      });
+      tableHTML += `</table>`;
+      let variables = { port: portNumber, table: tableHTML };
+      response.render("viewAll", variables);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await client.close();
+    }
+  })();
+});
+
+//-------remove one pokemon---------------------------------------------------
+app.post("/removeOne", function (request, response) {
+  (async () => {
+    try {
+      await client.connect();
+      console.log("***** Deleting one movie *****");
+      let targetName = request.body.name;
+      await deleteOne(client, databaseAndCollection, targetName);
+      let variables = { port: portNumber, numApp: 1 };
+      response.render("removeConfirm", variables);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await client.close();
+    }
+  })();
+});
+
+async function deleteOne(client, databaseAndCollection, targetName) {
+  let filter = { name: targetName };
+  const result = await client
+    .db(databaseAndCollection.db)
+    .collection(databaseAndCollection.collection)
+    .deleteOne(filter);
+
+  console.log(`Documents deleted ${result.deletedCount}`);
+}
+
+//-------Remove all pokemon---------------------------------------------------
+app.get("/removeApp", function (request, response) {
+  let variables = { port: portNumber, port1: portNumber };
+
+  response.render("remove", variables);
+});
+
+app.post("/removeConfirm", function (request, response) {
+  //get number of entries in collection
+  //remove all elements
+  (async () => {
+    try {
+      await client.connect();
+      const result = await client
+        .db(databaseAndCollection.db)
+        .collection(databaseAndCollection.collection)
+        .deleteMany({});
+      let variables = {
+        numApp: result.deletedCount,
+        port: portNumber,
+      };
+
+      response.render("removeConfirm", variables);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await client.close();
+    }
+  })();
+});
+
+//-----terminal stuff---------------------------------------------------------
 http.createServer(app).listen(portNumber);
 process.stdout.write(
   `Web server started and running at http://localhost:${portNumber}\n`
